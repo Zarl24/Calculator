67594b2306b8421299b2c891eb67fb6b5f89749f
Zarl24 <161961553+Zarl24@users.noreply.github.com>
2024-03-02 19:27:33 +0300

    Add files via upload

---
+++ ConsoleApplication2.cpp
@@ -0,0 +1,44 @@
+﻿
+#include <iostream>
+#include <Windows.h>
+#include <time.h>
+using namespace std;
+
+int main()
+{
+    SetConsoleCP(1251);
+    setlocale(LC_ALL, "Russian");
+
+    float num1, num2, res;
+    char op;
+
+    cout << "Введите первое число: ";
+    cin >> num1;
+
+    cout << "Введите действие +, -, *, /: ";
+    cin >> op;
+
+    cout << "Введите второе число: ";
+    cin >> num2;
+
+    switch (op)
+    {
+    case '+':
+        res = num1 + num2;
+        break;
+    case '-':
+        res = num1 - num2;
+        break;
+    case '*':
+        res = num1 * num2;
+        break;
+    case '/':
+        res = num1 / num2;
+        break;
+    default:
+        res = 0; cout << "Ошибка!";
+        break;
+    }
+    cout << "Ответ: " << res << endl;
+    return 0;
+}
